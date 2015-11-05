0311d54cb5599436c924a5f13f24480cff64ed41
tcholakov <tcholakov2@gmail.com>
2015-11-05 10:36:24 +0200

    Added CrowSourcedNews server and database first

---
+++ CrowdSourcedNews/Data/CrowdSourcedNews.Data/CrowSourcedNewsDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace CrowdSourcedNews.Data
+{
+    using CrowdSourcedNews.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class CrowdSourcedNewsDbContext : IdentityDbContext<User>
+    {
+        public CrowdSourcedNewsDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static CrowdSourcedNewsDbContext Create()
+        {
+            return new CrowdSourcedNewsDbContext();
+        }
+    }
+}
---
+++ CrowdSourcedNews/Server/CrowdSourcedNews.Api/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿namespace CrowdSourcedNews.Api
+{
+    using App_Start;
+    using System.Web.Http;
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            DatabaseConfing.Initialize();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
