a72cd86525a40ef920d0dfc91b073bfae3913299
tcholakov <tcholakov2@gmail.com>
2015-11-19 13:51:27 +0200

    Uploading CommentResponseModel

---
+++ CrowdSourcedNews/Server/CrowdSourcedNews.Api/Models/Comment/CommentResponseModel.cs
@@ -0,0 +1,25 @@
+﻿namespace CrowdSourcedNews.Api.Models.Comment
+{
+    using Infrastructure.Mappings;
+    using CrowdSourcedNews.Models;
+    using System.Collections.Generic;
+    using AutoMapper;
+    using System;
+
+    public class CommentResponseModel : IMapFrom<NewsArticle>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+        
+        public string Content { get; set; }
+        
+        public string Author { get; set; }
+
+        public ICollection<Comment> SubComments { get; set; }
+
+        public void CreateMappings(IConfiguration config)
+        {
+            config.CreateMap<Comment, CommentResponseModel>()
+                .ForMember(c => c.Author, opts => opts.MapFrom(c => c.Author.UserName));
+        }
+    }
+}
