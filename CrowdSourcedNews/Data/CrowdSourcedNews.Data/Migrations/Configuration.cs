baa1697705bed290d863236ddd5dbfc1348e67f2
Konstantin Adamu <KonstantinAdamu@users.noreply.github.com>
2015-11-19 18:34:02 +0200

    Added categories initialization in a Seed method CrowdSourcedNewsDbContext

--- CrowdSourcedNews/Data/CrowdSourcedNews.Data/Migrations/Configuration.cs
+++ CrowdSourcedNews/Data/CrowdSourcedNews.Data/Migrations/Configuration.cs
@@ -2,6 +2,8 @@ namespace CrowdSourcedNews.Data.Migrations
 {
     using System.Data.Entity.Migrations;
 
+    using CrowdSourcedNews.Models;
+
     public sealed class Configuration : DbMigrationsConfiguration<CrowdSourcedNews.Data.CrowdSourcedNewsDbContext>
     {
         public Configuration()
@@ -12,18 +14,19 @@ namespace CrowdSourcedNews.Data.Migrations
 
         protected override void Seed(CrowdSourcedNewsDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Categories.AddOrUpdate(
+                  c => c.Name,
+                  new Category { Name = "Sports" },
+                  new Category { Name = "Politics" },
+                  new Category { Name = "Food" },
+                  new Category { Name = "International" },
+                  new Category { Name = "Local" },
+                  new Category { Name = "Business" },
+                  new Category { Name = "Economy" },
+                  new Category { Name = "Education" },
+                  new Category { Name = "Health" },
+                  new Category { Name = "Celebrity" }
+                  );
         }
     }
 }
